cf9d1daf33bb65f2f86226a6317d137967e8eefe
Pablo L. Garcia Rodriguez <pablolgr1@gmail.com>
2026-05-08 20:48:04 -0400

    fixed frontend env url

--- src/db/index.ts
+++ src/db/index.ts
@@ -13,6 +13,8 @@ if (!process.env.DATABASE_URL) {
 // Creamos la conexión
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  family: 4,
 });
 
 // Exportamos la instancia de la base de datos con el esquema
